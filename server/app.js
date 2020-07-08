0f12c70ea737b364ce3ca262894cc9fb90bf7c8e
waiyanphyohein <waiyanphyohein@gmail.com>
2020-07-08 12:49:49 -0400

    Added Mongoose and its related settings

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const schema = require('./schema/schema');
 const express = require('express');
-const port = 8080;
+const mongoose = require('mongoose');
 const graphqlHTTP = require('express-graphql');
+const MongooseSettting = require('./setting');
+const port = 8080;
 const app = express();
 
+
+// Connect To MLab DB
+mongoose.connect(MongooseSettting.connectionString, MongooseSettting.options);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to Database.');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
---
+++ server/setting.js
@@ -0,0 +1,8 @@
+// For Mongoose Setting and options during conenction establishment
+module.exports = {
+    connectionString: "mongodb://waiyanphyohein:WaiYan1997@ds135810.mlab.com:35810/heroku_kjpn8d59",
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
