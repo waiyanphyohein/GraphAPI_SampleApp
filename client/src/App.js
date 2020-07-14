7ed6ecc16043747ac5b74b3c5290e0d5e85ee3aa
waiyanphyohein <waiyanphyohein@gmail.com>
2020-07-13 22:52:27 -0400

    Added react frontend template

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Components
+import BookList from './components/BookList'
+
+function App() {
+  return (
+    <div id="main">      
+      <h1>
+        Ninja's Reading List.
+      </h1>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
